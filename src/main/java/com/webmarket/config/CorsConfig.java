5493008340609ff252d6ae617029244d0f411ae8
LEEjunswer <jun_5949@naver.com>
2025-07-01 15:42:49 +0900

    2025-07-01 cors 수정
    2025-07-01 cors 수정 cors 변경

--- src/main/java/com/webmarket/config/CorsConfig.java
+++ src/main/java/com/webmarket/config/CorsConfig.java
@@ -10,10 +10,9 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*")
                 .allowedOrigins("http://localhost:3000") /*본인의 아이피 설정*/
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*")
-                .allowCredentials(false);
+                .allowCredentials(true);
     }
 }
--- src/main/java/com/webmarket/dto/request/member/ValidCheckEmailRequestDTO.java
+++ src/main/java/com/webmarket/dto/request/member/ValidCheckEmailRequestDTO.java
@@ -3,14 +3,14 @@ package com.webmarket.dto.request.member;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 import org.hibernate.annotations.Comment;
 
 @Getter
+@Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@ToString
 public class ValidCheckEmailRequestDTO {
 
     @NotNull(message = "이메일은 널이 될 수 없습니다")
