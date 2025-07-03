f61dc69fe1ce3190f867f0cc46ff80b8e518b390
LEEjunswer <jun_5949@naver.com>
2025-07-03 10:58:33 +0900

    2025-07-03 시큐리티컨피그 수정
     2025-07-03 시큐리티컨피그 수정

--- src/main/java/com/webmarket/config/SecurityConfig.java
+++ src/main/java/com/webmarket/config/SecurityConfig.java
@@ -28,6 +28,7 @@ public class SecurityConfig {
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(
                                 "/auth/login",
+                                "/auth/socialLogin",
                                 "/member/validCheckEmail",
                                 "/member/insertMember",
                                 "/member/validCheckNick",
